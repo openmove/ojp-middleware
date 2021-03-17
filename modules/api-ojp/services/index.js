6c6a5f4de04b3857105ffb2d59d2e1bafb97482d
stefanocudini <stefano.cudini@gmail.com>
2021-03-17 13:29:37 +0100

    api ojp

--- modules/api-ojp/services/index.js
+++ modules/api-ojp/services/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const https = require('https');
 const _ = require('lodash');
-const csv2json = require('csvtojson');
-const config = require('./config');
+const config = require('utils/config');
 
 const app = express();
 
 console.log("Start API_OJP...")
 
 console.log("Config:\n", config);
+
+
+
+//TODO use express
+//
+
+//TODO entrypoints
+/*
+OJPLocationInformation
+OJPTrip
+OJPStopEvent
+OJPTripInfo
+OJPExchangePoints
+OJPMultiPointTrip
+*/
