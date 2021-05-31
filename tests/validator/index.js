df0a2bff0c814af9a409af55ea2ae2c949e26ecf
stefanocudini <stefano.cudini@gmail.com>
2021-05-31 18:16:10 +0200

    validator

---
+++ tests/validator/index.js
@@ -0,0 +1,38 @@
+
+const fs = require('fs')
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+//const xmlparser = require('express-xml-bodyparser');
+const validator = require('xsd-schema-validator');
+
+const port = 5000;
+
+//app.use(bodyParser.urlencoded({ extended: false }))
+
+//app.use(bodyParser.raw({ type: 'application/xml' }))
+app.use(bodyParser.text({ type: 'application/xml' }))
+//  http://expressjs.com/en/resources/middleware/body-parser.html#bodyparserrawoptions
+
+app.post('/OJPLocationInformationRequest/', (req, res) => {
+  
+  console.log('REQUEST',req.body)
+
+  validator.validateXML(req.body, 'resources/foo.xsd', function(err, result) {
+    if (err) {
+      throw err;
+    }
+
+    result.valid; // true
+
+    res.send(req.body)
+  });
+
+  
+
+});
+
+app.listen(port, () => {
+  console.log(`API OTP service running on port ${port}`)
+})
