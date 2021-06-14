55cf31b0ec2729b115e3125a9009092d947bdc55
stefanocudini <stefano.cudini@gmail.com>
2021-06-14 12:45:15 +0200

    export db, ignore csvs

--- modules/db/export.sh
+++ modules/db/export.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #
-docker exec db sh -c 'exec mongodump -d ojp --archive' > ojp.archive
+VERSION=$(node -p -e "require('../../lerna.json').version");
+
+echo $VERSION
+
+docker exec db sh -c 'exec mongodump -d ojp --archive' > dumps/${VERSION}.archive
