c81bebd0b51ae362136066f7baa5551884993ee7
stefanocudini <stefano.cudini@gmail.com>
2021-10-14 14:54:18 +0200

    add new module response,errors

---
+++ modules/api-ojp/lib/response.js
@@ -0,0 +1,13 @@
+
+const xmlbuilder = require('xmlbuilder');
+const _ = require('lodash');
+
+module.exports = {
+  'createResponse': (stops, startTime, ptModes) => {
+
+  },
+
+  'createErrorResponse': (errorCode, startTime) => {
+
+  }
+}
