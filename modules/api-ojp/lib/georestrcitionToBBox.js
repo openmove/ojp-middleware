819803e6c63131ee9a70fb424485b7f02e54493f
stefanocudini <stefano.cudini@gmail.com>
2022-08-29 14:19:35 +0200

    parser tester

--- modules/api-ojp/lib/georestrcitionToBBox.js
+++ modules/api-ojp/lib/georestrcitionToBBox.js
@@ -3,14 +3,26 @@ const fs = require('fs');
 
 const { parseGeoRestriction } = require('./restrictions');
 
+const serviceTag = 'OJPLocationInformationRequest';
 
-const data = fs.readFileSync(0, 'utf-8');//stdin
+const xml = fs.readFileSync(0, 'utf-8');//stdin
+
+doc = new DOMParser({
+      errorHandler:{
+        warning: err => {
+          console.warn('WARNING XML PARSING', err);
+        },
+        error: err => {
+          console.warn('ERROR XML PARSING',err)
+        }
+      }
+    }).parseFromString(xml);
+
+const geores = parseGeoRestriction(doc, serviceTag);
 
 const { rect, upperLon, upperLat, lowerLon, lowerLat
-	, circle, radius, centerLon, centerLat } = parseGeoRestriction(doc, serviceTag);
+	, circle, radius, centerLon, centerLat } = geores;
 
+console.log(geores);
 
-
-
-
-
+console.log(`http://bboxfinder.com/#${lowerLat},${lowerLon},${upperLat},${upperLon}`);
